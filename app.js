d8c0d3871d511ec2195d90e09e0984b81204cfb5
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-01-27 01:48:33 -0700

    [cloudCafe]--render data in list

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const cafeList = document.querySelector('#cafe-list');
+//create list element and render data
+function renderCafe(doc){
+  let li = document.createElement('li');
+  let name=document.createElement('span');
+  let city=document.createElement('span');
+ 
+  li.setAttribute('data-id', doc.id);  //id is not save in database, it is not the part of data
+  name.textContent=doc.data().name;
+  city.textContent=doc.data().city;
+
+  //assign value to li tag
+  li.appendChild(name);
+  li.appendChild(city);
+  
+
+//assign the child to ul.
+cafeList.appendChild(li);
+
+}
+
 db.collection('cafes').get().then((snapshot)=>{
   snapshot.docs.forEach(doc=> {
-    console.log(doc.data());
-    
-  });
+    renderCafe(doc);    
+  })
 })
